9fad98d514503d447e7b841a2d0de57b9cecdcc9
Biser Atanasov <biser.p.atanasov@gmail.com>
2026-09-16 20:49:47 +0300

    openport-j2534 0.1.2

--- Formula/openport-j2534.rb
+++ Formula/openport-j2534.rb
@@ -1,8 +1,8 @@
 class OpenportJ2534 < Formula
   desc "SAE J2534 PassThru driver for the Tactrix OpenPort 2.0 (macOS, Linux)"
   homepage "https://github.com/bisak/openport-j2534"
-  url "https://github.com/bisak/openport-j2534/archive/refs/tags/v0.1.1.tar.gz"
-  sha256 "29e1487456944c88d7c369b34dba5133ddf04e0e314e9cd6ddf5e809158c1d0b"
+  url "https://github.com/bisak/openport-j2534/archive/refs/tags/v0.1.2.tar.gz"
+  sha256 "31ae08726510e9c8d26ea3239025ff176934d1b3a16b5af06b7396a8157a082c"
   license "GPL-3.0-or-later"
   head "https://github.com/bisak/openport-j2534.git", branch: "main"
 
